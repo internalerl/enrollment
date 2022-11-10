23f3b8bfc9501f3c43c1bafc59accf6d156d67f9
Earl Jeff <earldalugdugan@gmail.com>
2022-11-10 22:33:59 +0800

    innit commit

--- enrollment/pages/index.vue
+++ enrollment/pages/index.vue
@@ -39,9 +39,9 @@
               <button class="btn btn-outline-light btn-lg px-5" type="submit">LOGIN</button>
 
               <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg">facebook</i></a>
-                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2">twitter</i></a>
-                <a href="#!" class="text-white"><i class="fab fa-google fa-lg">google</i></a>
+                <a href="https://www.facebook.com/" class="text-white"><i class="fab fa-facebook-f fa-lg">facebook</i></a>
+                <a href="https://twitter.com/home" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2">twitter</i></a>
+                <a href="https://www.google.com/" class="text-white"><i class="fab fa-google fa-lg">google</i></a>
               </div>
 
             <div>
